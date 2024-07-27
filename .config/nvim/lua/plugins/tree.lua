2ca0a03cc9376e15c878edcc5522effc39103bf5
Ali Atashrooz <atashroozarash@gmail.com>
2024-07-27 23:22:29 +0330

    Update

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -1,3 +1,24 @@
 return {
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local function my_on_attach(bufnr)
+			local api = require "nvim-tree.api"
 
+			local function opts(desc)
+				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
+
+			vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+			vim.keymap.set('n', '<C-n>',     api.tree.toggle,                  opts('Help'))
+		end
+
+		require("nvim-tree").setup {
+			on_attach = my_on_attach,
+		}
+	end
 }
