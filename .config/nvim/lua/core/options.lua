5a643d59cb79618cc61ffd59d1616d773de259ed
Ali Atashrooz <atashroozarash@gmail.com>
2024-07-31 20:13:30 +0330

    Update

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,6 +1,11 @@
 vim.g.mapleader = ';'
 
 vim.wo.number = true
+
+-- disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.opt.termguicolors = true
 
 vim.api.nvim_set_option("clipboard", "unnamedplus")
