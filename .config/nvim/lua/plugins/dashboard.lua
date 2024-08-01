98b59545591391ea6df841e6751507d7d58a98e6
Ali Atashrooz <atashroozarash@gmail.com>
2024-08-01 14:10:28 +0330

    Update

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,21 +1,21 @@
 return {
-	'nvimdev/dashboard-nvim',
-	event = 'VimEnter',
-	dependencies = { {'nvim-tree/nvim-web-devicons'}, {'nvim-telescope/telescope.nvim'}},
+	"nvimdev/dashboard-nvim",
+	event = "VimEnter",
+	dependencies = { { "nvim-tree/nvim-web-devicons" }, { "nvim-telescope/telescope.nvim" } },
 	config = function()
-		require('dashboard').setup({
-			theme = 'hyper',
+		require("dashboard").setup({
+			theme = "hyper",
 			config = {
 				week_header = {
 					enable = true,
 				},
 				shortcut = {
-					{ desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+					{ desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
 					{
-						desc = ' Dotfiles',
-						group = 'Number',
-						action = 'Telescope dotfiles',
-						key = 'd',
+						desc = " Dotfiles",
+						group = "Number",
+						action = "e ~/.config/nvim/",
+						key = "d",
 					},
 				},
 			},
