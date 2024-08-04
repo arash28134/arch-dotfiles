b6811672b3b0a5edcce1518179b1275264cc1e3a
Ali Atashrooz <atashroozarash@gmail.com>
2024-08-05 00:08:39 +0330

    Update

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,19 +1,4 @@
 return {
-	{ "folke/tokyonight.nvim" },
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v3.x",
-		config = function()
-			local lsp_zero = require("lsp-zero")
-
-			lsp_zero.on_attach(function(client, bufnr)
-				lsp_zero.default_keymaps({ buffer = bufnr })
-			end)
-			lsp_zero.setup_servers({ "clangd" })
-		end,
-	},
-	{ "neovim/nvim-lspconfig" },
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/nvim-cmp" },
-	{ "L3MON4D3/LuaSnip" },
+	"neoclide/coc.nvim",
+	branch = "release",
 }
