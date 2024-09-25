71a07169e56aa158ef81b30864f2a9010f23c3cd
Ali Atashrooz <atashroozarash@gmail.com>
2024-09-26 00:41:12 +0330

    Update

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -1,12 +1,12 @@
 return {
-	-- "sbdchd/neoformat",
-	-- config = function()
-	-- 	vim.g.neoformat_enabled_cpp = { "astyle" }
-	-- 	vim.g.neoformat_enabled_c = { "astyle" }
-	--
-	-- 	local autocmd = vim.api.nvim_create_autocmd
-	-- 	autocmd("BufWritePre", {
-	-- 		command = ":Neoformat",
-	-- 	})
-	-- end,
+	"sbdchd/neoformat",
+	config = function()
+		vim.g.neoformat_enabled_cpp = { "astyle" }
+		vim.g.neoformat_enabled_c = { "astyle" }
+
+		-- local autocmd = vim.api.nvim_create_autocmd
+		-- autocmd("BufWritePre", {
+		-- 	command = ":Neoformat",
+		-- })
+	end,
 }
